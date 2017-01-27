ec626ecd02739e23007c53ba8a99ac096c69aad7
Simon Richards <s.richards@dewynters.com>
2017-01-27 23:52:13 +0000

    'initial'

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBp7inbp7e4_vOsfZA0weVwBXbs_l-ojHk ';
+
+
+class App extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'ass thong'}, (videos) => {
+			this.setState({ videos });
+		});
+
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);		
+	}
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
